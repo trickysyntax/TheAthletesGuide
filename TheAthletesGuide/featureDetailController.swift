67b70d9eb33a1e0c1389d9ffdf38efc63298b2d1
trickysyntax <josh@ciderhouse.org>
2019-07-22 13:35:38 -0700

    Trimmed code by 1/3, reorganized Storyboard

--- TheAthletesGuide/featureDetailController.swift
+++
@@ -1,9 +0,0 @@
-//
-//  featureDetailController.swift
-//  TheAthletesGuide
-//
-//  Created by Developer on 7/11/19.
-//  Copyright © 2019 Ciderhouse. All rights reserved.
-//
-
-import Foundation
--- TheAthletesGuide/xCityModel.swift
+++
@@ -1,9 +0,0 @@
-//
-//  xCityModel.swift
-//  TheAthletesGuide
-//
-//  Created by Developer on 5/30/19.
-//  Copyright © 2019 Ciderhouse. All rights reserved.
-//
-
-import Foundation
